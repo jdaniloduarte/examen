8b1d67797c7721ddce3473845e6518c82cedcde6
jdaniloduarte <70676970+jdaniloduarte@users.noreply.github.com>
2020-12-14 22:00:50 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,33 @@
+// Initialize your app
+var myApp = new Framework7({
+	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: false
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'My App',
+	dialog: {
+		title: 'My App',
+		buttonOk: 'Aceptar',
+  	},
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
